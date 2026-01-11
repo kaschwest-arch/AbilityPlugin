fa234927f84e7af7ee3a2481ca01b2c9e4725e03
kaschwest-arch <kasch.west@gmail.com>
2026-01-10 19:59:55 -0600

    Update ThunderStep.java

--- src/main/java/com/example/abilities/ability/ThunderStep.java
+++ src/main/java/com/example/abilities/ability/ThunderStep.java
@@ -11,7 +11,7 @@ public class ThunderStep extends Ability {
         super(
             "Thunder Step",
             10,
-            "Dash forward\nwith lightning speed.",
+            "Lightning-fast dash.",
             Sound.ENTITY_LIGHTNING_BOLT_IMPACT,
             Particle.ELECTRIC_SPARK
         );
@@ -20,9 +20,8 @@ public class ThunderStep extends Ability {
     @Override
     public void activate(Player player) {
         playEffects(player);
-
-        Vector v = player.getLocation().getDirection().multiply(2.2);
-        v.setY(0.25);
-        player.setVelocity(v);
+        player.setVelocity(
+                player.getLocation().getDirection().multiply(2.5)
+        );
     }
 }
