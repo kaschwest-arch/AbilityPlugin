6fd55f10593141536bda7b6e5fd8dc0681e103bf
kaschwest-arch <kasch.west@gmail.com>
2026-01-10 22:42:42 -0600

    Add files via upload

---
+++ src/main/java/com/example/abilities/command/AbilitiesCommand.java
@@ -0,0 +1,21 @@
+package com.example.abilities.command;
+
+import com.example.abilities.gui.AbilityGUI;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+public class AbilitiesCommand implements CommandExecutor {
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if (!(sender instanceof Player player)) {
+            sender.sendMessage("Only players can use this command.");
+            return true;
+        }
+
+        AbilityGUI.open(player);
+        return true;
+    }
+}
