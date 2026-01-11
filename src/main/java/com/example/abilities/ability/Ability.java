4a42a6a1834048eb8b63f3e562789e04519bcb80
kaschwest-arch <kasch.west@gmail.com>
2026-01-10 20:24:21 -0600

    Update Ability.java

--- src/main/java/com/example/abilities/ability/Ability.java
+++ src/main/java/com/example/abilities/ability/Ability.java
@@ -1,25 +1,46 @@
 package com.example.abilities.ability;
 
-import org.bukkit.Material;
+import org.bukkit.Particle;
+import org.bukkit.Sound;
 import org.bukkit.entity.Player;
 
 public abstract class Ability {
-    private final String name;
-    private final String permission;
-    private final int cooldown;
-    private final Material icon;
 
-    public Ability(String name, String permission, int cooldown, Material icon) {
+    private final String name;
+    private final int cooldown;
+    private final String description;
+    private final Sound sound;
+    private final Particle particle;
+
+    public Ability(String name, int cooldown, String description, Sound sound, Particle particle) {
         this.name = name;
-        this.permission = permission;
         this.cooldown = cooldown;
-        this.icon = icon;
+        this.description = description;
+        this.sound = sound;
+        this.particle = particle;
     }
 
-    public String getName() { return name; }
-    public String getPermission() { return permission; }
-    public int getCooldown() { return cooldown; }
-    public Material getIcon() { return icon; }
-
     public abstract void activate(Player player);
+
+    public String getName() {
+        return name;
+    }
+
+    public int getCooldown() {
+        return cooldown;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    protected void playEffects(Player player) {
+        player.playSound(player.getLocation(), sound, 1f, 1f);
+        player.getWorld().spawnParticle(
+            particle,
+            player.getLocation().add(0, 1, 0),
+            30,
+            0.5, 0.5, 0.5
+        );
+    }
 }
