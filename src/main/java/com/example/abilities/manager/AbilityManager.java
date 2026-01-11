59770d7a226ac5c2876a58dd6b6b28027baf54cc
kaschwest-arch <kasch.west@gmail.com>
2026-01-10 21:02:11 -0600

    Update AbilityManager.java

--- src/main/java/com/example/abilities/manager/AbilityManager.java
+++ src/main/java/com/example/abilities/manager/AbilityManager.java
@@ -1,36 +1,45 @@
 package com.example.abilities.manager;
 
 import com.example.abilities.ability.*;
-import org.bukkit.entity.Player;
-
 import java.util.ArrayList;
 import java.util.List;
 
 public class AbilityManager {
 
-    private static final List<Ability> abilities = new ArrayList<>();
+    private final List<Ability> abilities = new ArrayList<>();
 
-    public static void registerAbilities() {
-        abilities.add(new ArcaneShield());
-        abilities.add(new Blink());
-        abilities.add(new ManaSurge());
-        // add others here
+    public AbilityManager() {
+        registerAbilities();
     }
 
-    public static List<Ability> getAbilities() {
+    private void registerAbilities() {
+        abilities.add(new ArcaneShield());
+        abilities.add(new Blink());
+        abilities.add(new EarthWall());
+        abilities.add(new FireDash());
+        abilities.add(new FlameWave());
+        abilities.add(new FrostNova());
+        abilities.add(new HealingAura());
+        abilities.add(new LightningStrike());
+        abilities.add(new ManaSurge());
+        abilities.add(new MeteorFall());
+        abilities.add(new PhantomCloak());
+        abilities.add(new ThunderStep());
+        abilities.add(new TimeSlow());
+        abilities.add(new VoidPull());
+        abilities.add(new WindLeap());
+    }
+
+    public List<Ability> getAbilities() {
         return abilities;
     }
 
-    public static Ability getAbilityByName(String name) {
-        for (Ability a : abilities) {
-            if (a.getName().equalsIgnoreCase(name)) {
-                return a;
+    public Ability getAbilityByName(String name) {
+        for (Ability ability : abilities) {
+            if (ability.getName().equalsIgnoreCase(name)) {
+                return ability;
             }
         }
         return null;
-    }
-
-    public static void activate(Player player, Ability ability) {
-        ability.activate(player);
     }
 }
