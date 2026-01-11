fa83c59cd5ff43584bc54efc43dffd4c8619965a
kaschwest-arch <kasch.west@gmail.com>
2026-01-10 19:57:33 -0600

    Update Blink.java

--- src/main/java/com/example/abilities/ability/Blink.java
+++ src/main/java/com/example/abilities/ability/Blink.java
@@ -3,15 +3,14 @@ package com.example.abilities.ability;
 import org.bukkit.Particle;
 import org.bukkit.Sound;
 import org.bukkit.entity.Player;
-import org.bukkit.util.Vector;
 
 public class Blink extends Ability {
 
     public Blink() {
         super(
             "Blink",
-            10,
-            "Teleports you a short\ndistance forward.",
+            6,
+            "Teleport a short\ndistance forward.",
             Sound.ENTITY_ENDERMAN_TELEPORT,
             Particle.PORTAL
         );
@@ -20,8 +19,8 @@ public class Blink extends Ability {
     @Override
     public void activate(Player player) {
         playEffects(player);
-
-        Vector direction = player.getLocation().getDirection().normalize().multiply(6);
-        player.teleport(player.getLocation().add(direction));
+        player.teleport(player.getLocation().add(
+                player.getLocation().getDirection().multiply(6)
+        ));
     }
 }
