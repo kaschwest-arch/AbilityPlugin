f264db3d56a4f71be374159639bbd7e7c3bcc43e
kaschwest-arch <kasch.west@gmail.com>
2026-01-10 19:54:25 -0600

    Update PhantomCloak.java

--- src/main/java/com/example/abilities/ability/PhantomCloak.java
+++ src/main/java/com/example/abilities/ability/PhantomCloak.java
@@ -1,18 +1,33 @@
 package com.example.abilities.ability;
 
-import org.bukkit.*;
+import org.bukkit.Particle;
+import org.bukkit.Sound;
 import org.bukkit.entity.Player;
 import org.bukkit.potion.PotionEffect;
 import org.bukkit.potion.PotionEffectType;
-import org.bukkit.util.Vector;
 
 public class PhantomCloak extends Ability {
+
     public PhantomCloak() {
-        super("PhantomCloak", "abilities.phantom_cloak", 22, Material.PHANTOM_MEMBRANE);
+        super(
+            "Phantom Cloak",
+            22,
+            "Turns you invisible\nbriefly.",
+            Sound.ENTITY_PHANTOM_FLAP,
+            Particle.SMOKE
+        );
     }
 
     @Override
     public void activate(Player player) {
-        player.addPotionEffect(new PotionEffect(PotionEffectType.INVISIBILITY,100,0));
+        playEffects(player);
+
+        player.addPotionEffect(new PotionEffect(
+                PotionEffectType.INVISIBILITY,
+                120,
+                0,
+                true,
+                false
+        ));
     }
 }
