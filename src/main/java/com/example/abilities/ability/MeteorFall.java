fcbbb2058a185a99a1e37343c1ab5f33d970d296
kaschwest-arch <kasch.west@gmail.com>
2026-01-10 19:59:24 -0600

    Update MeteorFall.java

--- src/main/java/com/example/abilities/ability/MeteorFall.java
+++ src/main/java/com/example/abilities/ability/MeteorFall.java
@@ -9,8 +9,8 @@ public class MeteorFall extends Ability {
     public MeteorFall() {
         super(
             "Meteor Fall",
-            25,
-            "Summons a meteor\nfrom the sky.",
+            28,
+            "Explodes target\narea.",
             Sound.ENTITY_GENERIC_EXPLODE,
             Particle.EXPLOSION
         );
@@ -19,15 +19,14 @@ public class MeteorFall extends Ability {
     @Override
     public void activate(Player player) {
         playEffects(player);
-
-        if (player.getTargetBlockExact(30) == null) return;
-
-        player.getWorld().createExplosion(
-                player.getTargetBlockExact(30).getLocation(),
-                3.0f,
-                false,
-                false,
-                player
-        );
+        if (player.getTargetBlockExact(25) != null) {
+            player.getWorld().createExplosion(
+                    player.getTargetBlockExact(25).getLocation(),
+                    3f,
+                    false,
+                    false,
+                    player
+            );
+        }
     }
 }
