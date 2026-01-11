17b1cb3502c01157f097954c8bae3df8ef42300c
kaschwest-arch <kasch.west@gmail.com>
2026-01-10 19:55:02 -0600

    Update TimeSlow.java

--- src/main/java/com/example/abilities/ability/TimeSlow.java
+++ src/main/java/com/example/abilities/ability/TimeSlow.java
@@ -1,18 +1,38 @@
 package com.example.abilities.ability;
 
-import org.bukkit.*;
+import org.bukkit.Particle;
+import org.bukkit.Sound;
+import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
 import org.bukkit.potion.PotionEffect;
 import org.bukkit.potion.PotionEffectType;
-import org.bukkit.util.Vector;
 
 public class TimeSlow extends Ability {
+
     public TimeSlow() {
-        super("TimeSlow", "abilities.time_slow", 20, Material.CLOCK);
+        super(
+            "Time Slow",
+            18,
+            "Slows enemies\naround you.",
+            Sound.BLOCK_AMETHYST_BLOCK_CHIME,
+            Particle.PORTAL
+        );
     }
 
     @Override
     public void activate(Player player) {
-        player.getNearbyEntities(5,5,5).forEach(e->{if(e instanceof Player p)p.addPotionEffect(new PotionEffect(PotionEffectType.SLOWNESS,60,0));});
+        playEffects(player);
+
+        for (Entity e : player.getNearbyEntities(6, 3, 6)) {
+            if (e instanceof Player target && !target.equals(player)) {
+                target.addPotionEffect(new PotionEffect(
+                        PotionEffectType.SLOWNESS,
+                        100,
+                        4,
+                        true,
+                        false
+                ));
+            }
+        }
     }
 }
