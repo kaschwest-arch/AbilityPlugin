0f23841ddfb4f35cd7751b4255f1d93d03dafe0f
kaschwest-arch <kasch.west@gmail.com>
2026-01-10 19:55:33 -0600

    Update WindLeap.java

--- src/main/java/com/example/abilities/ability/WindLeap.java
+++ src/main/java/com/example/abilities/ability/WindLeap.java
@@ -1,18 +1,26 @@
 package com.example.abilities.ability;
 
-import org.bukkit.*;
+import org.bukkit.Particle;
+import org.bukkit.Sound;
 import org.bukkit.entity.Player;
-import org.bukkit.potion.PotionEffect;
-import org.bukkit.potion.PotionEffectType;
 import org.bukkit.util.Vector;
 
 public class WindLeap extends Ability {
+
     public WindLeap() {
-        super("WindLeap", "abilities.wind_leap", 5, Material.FEATHER);
+        super(
+            "Wind Leap",
+            7,
+            "Launches you high\ninto the air.",
+            Sound.ENTITY_PHANTOM_FLAP,
+            Particle.CLOUD
+        );
     }
 
     @Override
     public void activate(Player player) {
-        player.setVelocity(new Vector(0,1.2,0));
+        playEffects(player);
+
+        player.setVelocity(new Vector(0, 1.1, 0));
     }
 }
