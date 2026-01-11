019d98fc40101cd5e225422897e2e0f62eba72ff
kaschwest-arch <kasch.west@gmail.com>
2026-01-10 19:45:32 -0600

    Update AbilityPlugin.java

--- src/main/java/com/example/abilities/AbilityPlugin.java
+++ src/main/java/com/example/abilities/AbilityPlugin.java
@@ -1,14 +1,48 @@
-package com.example.abilities;
+package com.example.abilities.ability;
 
-import com.example.abilities.manager.AbilityManager;
-import com.example.abilities.gui.AbilityGUI;
-import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.Particle;
+import org.bukkit.Sound;
+import org.bukkit.entity.Player;
 
-public class AbilityPlugin extends JavaPlugin {
-    @Override
-    public void onEnable() {
-        AbilityManager manager = new AbilityManager();
-        getServer().getPluginManager().registerEvents(manager, this);
-        getServer().getPluginManager().registerEvents(new AbilityGUI(manager), this);
+public abstract class Ability {
+
+    private final String name;
+    private final int cooldownSeconds;
+    private final String description;
+    private final Sound sound;
+    private final Particle particle;
+
+    public Ability(String name, int cooldownSeconds, String description,
+                   Sound sound, Particle particle) {
+        this.name = name;
+        this.cooldownSeconds = cooldownSeconds;
+        this.description = description;
+        this.sound = sound;
+        this.particle = particle;
     }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getCooldownSeconds() {
+        return cooldownSeconds;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void playEffects(Player player) {
+        player.playSound(player.getLocation(), sound, 1.0f, 1.0f);
+        player.getWorld().spawnParticle(
+                particle,
+                player.getLocation().add(0, 1, 0),
+                30,
+                0.5, 0.5, 0.5,
+                0.05
+        );
+    }
+
+    public abstract void activate(Player player);
 }
