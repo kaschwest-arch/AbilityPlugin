38bfb83f99093f41cbbbd184d8727d952e317463
kaschwest-arch <kasch.west@gmail.com>
2026-01-10 19:48:47 -0600

    Update FireDash.java

--- src/main/java/com/example/abilities/ability/FireDash.java
+++ src/main/java/com/example/abilities/ability/FireDash.java
@@ -1,18 +1,28 @@
 package com.example.abilities.ability;
 
-import org.bukkit.*;
+import org.bukkit.Particle;
+import org.bukkit.Sound;
 import org.bukkit.entity.Player;
-import org.bukkit.potion.PotionEffect;
-import org.bukkit.potion.PotionEffectType;
 import org.bukkit.util.Vector;
 
 public class FireDash extends Ability {
+
     public FireDash() {
-        super("FireDash", "abilities.fire_dash", 6, Material.BLAZE_POWDER);
+        super(
+            "Fire Dash",
+            8,
+            "Launches you forward\nin a burst of fire.",
+            Sound.ENTITY_BLAZE_SHOOT,
+            Particle.FLAME
+        );
     }
 
     @Override
     public void activate(Player player) {
-        player.setVelocity(player.getLocation().getDirection().multiply(1.6));
+        playEffects(player);
+
+        Vector direction = player.getLocation().getDirection().normalize().multiply(1.8);
+        direction.setY(0.35);
+        player.setVelocity(direction);
     }
 }
