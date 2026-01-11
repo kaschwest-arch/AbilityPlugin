4cee624ac7c9d0a9a721128332c21f70ad8636f8
kaschwest-arch <kasch.west@gmail.com>
2026-01-10 20:01:30 -0600

    Update ManaSurge.java

--- src/main/java/com/example/abilities/ability/ManaSurge.java
+++ src/main/java/com/example/abilities/ability/ManaSurge.java
@@ -1,5 +1,6 @@
 package com.example.abilities.ability;
 
+import com.example.abilities.cooldown.CooldownManager;
 import org.bukkit.Particle;
 import org.bukkit.Sound;
 import org.bukkit.entity.Player;
@@ -10,15 +11,11 @@ public class ManaSurge extends Ability {
         super(
             "Mana Surge",
             30,
-            "Instantly refreshes\nability cooldowns.",
+            "Instantly refreshes\nall ability cooldowns.",
             Sound.BLOCK_BEACON_POWER_SELECT,
             Particle.END_ROD
         );
     }
 
     @Override
-    public void activate(Player player) {
-        playEffects(player);
-        // Hook into cooldown manager later
-    }
-}
+    public v
